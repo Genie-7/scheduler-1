85549eadf02a413b167897192000988705074e17
Chengyue Wei <weiyuezhong2012@gmail.com>
2023-02-14 11:20:55 -0500

    color change effect on meeting

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ var createEventButton = document.getElementById("create-event");
 createEventButton.addEventListener("click", function () {
   // Show the modal when the button is clicked
   document.getElementById("event-modal").style.display = "block";
+  // click outside the modal to close it
+  window.onclick = function (event) {
+    if (event.target == document.getElementById("event-modal")) {
+      document.getElementById("event-modal").style.display = "none";
+    }
+  }
 });
 
 // Get the event form
@@ -89,6 +95,18 @@ eventForm.addEventListener("submit", function (event) {
 
   // Hide the modal
   document.getElementById("event-modal").style.display = "none";
+
+  // save the event to local storage after the form is submitted
+  var event = {
+    name: eventName,
+    day: eventDay,
+    time: eventTime
+  };
+  var events = JSON.parse(localStorage.getItem("events")) || [];
+  events.push(event);
+  localStorage.setItem("events", JSON.stringify(events));
+
+
 });
 
 // Create the header row with weekdays
@@ -107,4 +125,50 @@ for (var i = 0; i < weekdays.length; i++) {
     var slotCell = timeRow.insertCell(j + 1);
     slotCell.innerHTML = "&nbsp;"; // Add a non-breaking space to the cell
   }
+}
+
+// drag and drop color change
+
+var colorOptions = document.querySelectorAll(".color-options .color");
+var tableCells = document.querySelectorAll("#calendar-table td");
+var removeOption = document.getElementById("remove-option");
+
+// Loop through the color options and add an event listener to each one
+for (var i = 0; i < colorOptions.length; i++) {
+  var colorOption = colorOptions[i];
+
+  colorOption.addEventListener("dragstart", function (event) {
+    // Set the data type and data for the drag operation
+    event.dataTransfer.setData("text/plain", this.classList[1]);
+  });
+}
+
+// Loop through the table cells and add an event listener to each one
+for (var i = 0; i < tableCells.length; i++) {
+  var tableCell = tableCells[i];
+
+  tableCell.addEventListener("dragover", function (event) {
+    event.preventDefault(); // Allow the drag operation to drop on the table cell
+  });
+
+  tableCell.addEventListener("drop", function (event) {
+    var colorClass = event.dataTransfer.getData("text/plain");
+    // checks if the element has a background color
+    if (this.classList.length > 0) {
+      // if it does, remove the background color
+      this.className = "";
+    }
+    this.classList.add(colorClass); // Add the color class to the table cell
+    event.preventDefault(); // Prevent the browser from opening the data as a link
+  });
+}
+
+// getItems from local storage and display them on the page when the page loads for the first time or when the page is refreshed
+var events = JSON.parse(localStorage.getItem("events")) || [];
+for (var i = 0; i < events.length; i++) {
+  var event = events[i];
+  var dayIndex = weekdays.indexOf(event.day);
+  var timeIndex = times.indexOf(event.time);
+  var eventCell = table.rows[dayIndex + 1].cells[timeIndex];
+  eventCell.innerHTML = event.name;
 }
